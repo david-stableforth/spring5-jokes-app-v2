5c5afe23d40331d8297d123c79ba8472e3ef2d44
David Stableforth <david.stableforth@gmail.com>
2024-10-10 22:54:05 -0400

    Created Joke Controller.  Closes #4

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring5jokesappv2.controllers;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * * Created by DS on 10/10/24
+ */
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
